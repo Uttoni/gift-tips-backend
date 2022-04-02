84bad6a60886923ce5eccc910974f8b18abfc6f3
Uttoni <uttoni.brandani@ges.inatel.br>
2022-04-02 15:55:09 -0300

    Creating base of post method at /home

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 var express  = require('express');
+var bodyParser = require("body-parser");
+
 var app = express();
 
-app.get("/profiles", (req, res, next) => {
-  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/home", (req, res) => {
+  res.send("Home page!")
 });
+
+app.post("/home", (req, res)=>{
+  console.log(req.body)
+  res.send(req.body)
+})
 
 app.listen(8000, () => {
   console.log("Server running on port 8000");
