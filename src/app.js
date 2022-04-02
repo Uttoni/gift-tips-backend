b4f6cb5b7e58e812e8e513e7657d2a9a8867f436
Uttoni <uttoni.brandani@ges.inatel.br>
2022-04-02 14:09:55 -0300

    Criando a base para fazer a api de criação do perfil do cliente

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var express  = require('express');
+var app = express();
+
+app.get("/profiles", (req, res, next) => {
+  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+});
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
--- src/controllers/ProfileController.js
+++
@@ -1,5 +0,0 @@
-module.exports =  (req, res) => {
-   
-    console.log("rezando o pai nosso")
-    res.send('Hey, tudo bom?');
-}
